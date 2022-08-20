09973843758b87ee4a255770f1da2a004c972948
isaccanedo <isaccanedo@gmail.com>
2022-08-20 11:27:54 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# Monacode
+
+Copy of https://monacode.live
+
+> A thin es-module wrapper around the monaco editor and prettier
+
+Exploring the feasibility of importing the [monaco-editor](https://github.com/Microsoft/monaco-editor) (which powers VS Code) and [prettier](https://github.com/prettier/prettier) modules directly into the browser; aiming to make it _one line easy_ to bring a native like code editing experiences to web applications without a build step.
+
+Ships with all the features usually found in VS Code out of the box: things like code hints, type inference, minimap, command pallet, multi-cursor select, find and replace, code folding etc.
+
+In addition to these features, prettier has been integrated so that hitting `⌘ + s` will format the code.
+
+<img width="100%" alt="monacode-demo" src="https://user-images.githubusercontent.com/1457604/100519495-1efed180-3190-11eb-9116-0bf1a619e08b.gif" />
+
+> ⚡️ Check out the hosted version at https://monacode.live
+
+## Usage
+
+Install the module as an npm module and import it using the bare module specifier, or import it directly from unpkg:
+
+```js
+import monacode from 'https://unpkg.com/monacode/index.min.js'; // 976Kb brotli
+
+// Create a new editor and attach to the document body
+const editor = monacode({
+  container: document.body,
+  value: 'const add = (x, y) => x + y;',
+});
+
+// Listen for changes within the editor
+editor.getModel().onDidChangeContent((change) => {
+  const newValue = editor.getValue();
+  console.log(newValue);
+});
+```
+
+It is also possible to embed the editor into your application using an iframe (see [demo](https://esm.codes/#Ly8gU2ltcGxlIG1vbmFjb2RlIGV4YW1wbGUgQGx1a2VqYWNrc29ubgovLyAtLS0tLS0tLS0tLS0tLS0tCiAgICAKaW1wb3J0IHsgcmVuZGVyLCBodG1sIH0gZnJvbSAnaHR0cHM6Ly9ucG0ucmV2ZXJzZWh0dHAuY29tL3ByZWFjdCxwcmVhY3QvaG9va3MsaHRtL3ByZWFjdCc7CgpyZW5kZXIoaHRtbGAKICA8aWZyYW1lCiAgICB3aWR0aD0iMTAwJSIKICAgIGhlaWdodD0iMTAwJSIKICAgIGZyYW1lQm9yZGVyPSIwIgogICAgc3JjPSJodHRwczovL21vbmFjb2RlLmxpdmU/dGhlbWU9dnMtZGFyayIKICAvPgpgLCBkb2N1bWVudC5ib2R5KQ==)):
+
+```html
+<iframe
+  src="https://monacode.live?theme=vs-light&value=console.log('hey')"
+></iframe>
+```
+
+Pass config options into the iframe as URL search parameters, making sure to URI encode values properly. Please note however, it is not currently possible to pass in nested config options.
+
+## API
+
+The module exports a single default function which accepts a config object as an argument. The supplied config gets merged with a [default config](index.js#L51) which itself is a subset of the options available to configure moanco editors. Some useful values to know include:
+
+- `container`: an HTML element that exists in the DOM (or a `ref` if using react/preact)
+- `value`: a string representing the initial code to be rendered within the editor
+- `language`: a string representing what language the editor should try syntax highlight and hint for
+- `fontSize`: a number indicating what pixel font size code should render at
+- `theme`: a string identifier of the desired theme or a url to a theme json file
+
+Calling the default export returns an instance of the underlying monaco editor. Find out more about how to interact with the editor by reading the [API documentation](https://microsoft.github.io/monaco-editor/api/index.html).
+
+## Development
+
+Running the following command from the root of this project uses [`servor`](https://github.com/lukejacksonn/servor) to start a dev server, then open your editor and browser on the appropriate localhost url.
+
+```bash
+npm start
+```
+
+## Production
+
+Running the following command from the root of this project uses [`esbuild`](https://github.com/evanw/esbuild) build the source to a single `index.min.js` file. Currently this process takes ~1second and results in an output file weighing 4.6MB (commpresses down to less than 1MB with brotli).
+
+```bash
+npm run build
+```
+
+## License
+
+Licensed under the [MIT](https://github.com/Microsoft/monaco-editor/blob/master/LICENSE.md) License.
---
+++ index.js
@@ -0,0 +1,161 @@
+import * as monaco from './src/monaco/editor/editor.main.js';
+
+import prettier from './src/prettier.js';
+import prettierBabel from './src/prettier-babel.js';
+
+const sheet = document.createElement('style');
+document.head.appendChild(sheet);
+
+sheet.innerHTML = '.monaco-editor { display: none; }';
+
+fetch('./src/index.css')
+  .then((res) => res.text())
+  .then((styles) => (sheet.innerHTML = styles));
+
+self.MonacoEnvironment = {
+  getWorkerUrl: function (moduleId, label) {
+    if (label === 'json') return './src/monaco/language/json/json.worker.js';
+    if (label === 'css') return './src/monaco/language/css/css.worker.js';
+    if (label === 'html') return './src/monaco/language/html/html.worker.js';
+    if (label === 'typescript' || label === 'javascript') {
+      return './src/monaco/language/typescript/ts.worker.js';
+    }
+    return './src/monaco/editor/editor.worker.js';
+  },
+};
+
+const computeOffset = (code, pos) => {
+  let line = 1;
+  let col = 1;
+  let offset = 0;
+  while (offset < code.length) {
+    if (line === pos.lineNumber && col === pos.column) return offset;
+    if (code[offset] === '\n') line++, (col = 1);
+    else col++;
+    offset++;
+  }
+  return -1;
+};
+
+const computePosition = (code, offset) => {
+  let line = 1;
+  let col = 1;
+  let char = 0;
+  while (char < offset) {
+    if (code[char] === '\n') line++, (col = 1);
+    else col++;
+    char++;
+  }
+  return { lineNumber: line, column: col };
+};
+
+const editorDefaults = {
+  value: '',
+  language: 'javascript',
+  theme: 'vs-dark',
+  formatOnType: false,
+  fontSize: 14,
+  tabSize: 2,
+  lineNumbersMinChars: 3,
+  minimap: {
+    enabled: true,
+  },
+  scrollbar: {
+    useShadows: false,
+  },
+  mouseWheelZoom: true,
+};
+
+export default (options) => {
+  const { container, ...restOfOptions } = options;
+
+  const editorOpts = {
+    ...editorDefaults,
+    ...restOfOptions,
+  }
+  console.log('editorOpts', editorOpts)
+
+  const editor = monaco.editor.create(container, editorOpts);
+
+  // Import themes directly from the amazing collection by @brijeshb42
+  // https://raw.githubusercontent.com/brijeshb42/monaco-themes/master/themes
+
+  if (options.theme === 'vs-light') container.style.backgroundColor = '#fff';
+  if (options.theme?.startsWith('http') || options.theme?.startsWith('./'))
+    fetch(options.theme)
+      .then((res) => res.json())
+      .then((data) => {
+        monaco.editor.defineTheme('theme', data);
+        monaco.editor.setTheme('theme');
+        container.style.backgroundColor = data.colors['editor.background'];
+      });
+
+  addEventListener('resize', function () {
+    editor.layout();
+  });
+
+  const alt = (e) => (navigator.platform.match('Mac') ? e.metaKey : e.ctrlKey);
+  const hotKeys = (e) => {
+    // Cdm + s formats with prettier
+    if (alt(e) && e.keyCode == 83) {
+      e.preventDefault();
+      const val = editor.getValue();
+      const pos = editor.getPosition();
+
+      let prettyVal = {}
+      if (editorOpts.language === 'json') {
+        prettyVal = {
+          formatted: JSON.stringify(JSON.parse(val), null, 2),
+          cursorOffset: computeOffset(val, pos)
+        }
+      } else {
+        console.log('val', val)
+        prettyVal = prettier.formatWithCursor(val, {
+          parser: 'babel',
+          plugins: prettierBabel,
+          semi: false,
+          singleQuote: true,
+          jsxSingleQuote: true,
+          printWidth: 100,
+          cursorOffset: computeOffset(val, pos),
+        });
+      }
+
+      editor.executeEdits('prettier', [
+        {
+          identifier: 'delete',
+          range: editor.getModel().getFullModelRange(),
+          text: '',
+          forceMoveMarkers: true,
+        },
+      ]);
+
+      editor.executeEdits('prettier', [
+        {
+          identifier: 'insert',
+          range: new monaco.Range(1, 1, 1, 1),
+          text: prettyVal.formatted,
+          forceMoveMarkers: true,
+        },
+      ]);
+
+      editor.setSelection(new monaco.Range(0, 0, 0, 0));
+      editor.setPosition(
+        computePosition(prettyVal.formatted, prettyVal.cursorOffset)
+      );
+    }
+    // Cmd + p opens the command palette
+    if (alt(e) && e.keyCode == 80) {
+      editor.trigger('anyString', 'editor.action.quickCommand');
+      e.preventDefault();
+    }
+    // Cmd + d prevents browser bookmark dialog
+    if (alt(e) && e.keyCode == 68) {
+      e.preventDefault();
+    }
+  };
+
+  container.addEventListener('keydown', hotKeys);
+
+  return editor;
+};
